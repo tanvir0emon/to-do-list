80c399d670c10fbbb11fe25e9cd8895c69a83fcc
tanvir0emon <tanvir.e61@gmail.com>
2020-12-15 20:50:37 +0600

    final

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const input = document.querySelector('.item-input');
+const todoId = document.querySelector('.id');
 const enter = document.querySelector('.enter');
 const list = document.querySelector('.item-list');
 const error = document.querySelector('.error');
 
-document.addEventListener('keypress', function(e){
-	if(e.key === "Enter"){
+let id = 1;
+
+document.addEventListener('keypress', function (e) {
+	if (e.key === "Enter") {
 		handleInput();
 	}
 })
@@ -13,52 +16,50 @@ enter.addEventListener('click', handleInput);
 
 
 
-function handleInput () {
+function handleInput() {
 	if (input.value == "") {
 		error.innerHTML = '<span class="text-danger">Item cannot be empty.</span>';
 	} else {
-		error.innerHTML = '';
-		let item = input.value
+		if (todoId.value == "") {
+			// New Todo
+			error.innerHTML = '';
+			let item = input.value
 
-		let todo = `<div class="d-flex justify-content-between border p-2 mb-2">
-						<div class="todo">${item}</div>
-						<div>
-							<span class="check text-success"  role="button"><i class="fas fa-check-circle"></i></span>
-							<span class="check text-info mx-3"  role="button"><i class="fas fa-edit"></i></span>
-							<span class="check text-danger"  role="button"><i class="fas fa-trash"></i></span>
-						</div>
-					</div>`;
+			let todo = `<div id="${id}" class="todo d-flex justify-content-between border p-2 mb-2 todo">
+							<div class="todo-text">${item}</div>
+							<div>
+								<span onclick="check(${id})" class="text-success"  role="button"><i class="fas fa-check-circle"></i></span>
+								<span onclick="edit(${id})" class="text-info mx-3"  role="button"><i class="fas fa-edit"></i></span>
+								<span onclick="trash(${id})" class="text-danger" role="button"><i class="fas fa-trash"></i></span>
+							</div>
+						</div>`;
 
-		list.insertAdjacentHTML('beforeend', todo);
-
-		// clear the input 
-		input.value = '';
-
-		// Check the to do
-		document.querySelectorAll('.check').forEach(function(checkBtn){
-			checkBtn.addEventListener('click', function(){
-				this.parentElement.previousElementSibling.classList.toggle('checked');
-			})
-		})
-
-		// Delete Cheque 
-
-		document.querySelectorAll('.trash').forEach(function(trashBtn){
-			trashBtn.addEventListener('click',function(){
-			
-			})
-
-		})
-
-
-
+			list.insertAdjacentHTML('beforeend', todo);
+			// clear the input 
+			input.value = '';
+			id++;
+		} else {
+			console.log('update todo');
+			let todo = document.getElementById(todoId.value).querySelector('.todo-text').innerText = input.value;
+			todoId.value="";
+			input.value="";
+		}
 
 
 	}
 }
 
-function removeToDo(element){
-	element.parentNode.parentNode.removeChild(element.parentNode);
-	
-	LIST[element.id].trash = true;
+function check(id) {
+	document.getElementById(id).classList.toggle('checked');
+}
+
+function trash(id) {
+	document.getElementById(id).remove();
+}
+
+function edit(id) {
+	let todoText = document.getElementById(id).querySelector('.todo-text');
+	input.value = todoText.innerText;
+	todoId.value = id;
+	input.focus();
 }
